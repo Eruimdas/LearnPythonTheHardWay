760057741bdc826a8ff717d9fa6bcce165e35c5a
Hasan Kemik <hhkcrazy@gmail.com>
2019-01-18 13:10:20 +0300

    Rearrange and exercises 21 to 43

---
+++ ex28/ex28.py
@@ -0,0 +1,20 @@
+# True
+# False
+# True and False -> False
+# True
+# True or True -> True
+# True and True -> True
+# False and False -> False
+# True or True -> True
+# False
+# True and False -> False
+# True
+# False
+# True
+# False
+# not(False or True) -> not(True) -> False
+# False
+# True
+# True
+# False
+# False
---
+++ ex34/ex34.py
@@ -0,0 +1,9 @@
+animals = ['bear', 'python3.6', 'peacock', 'kangaroo', 'whale', 'playtpus']
+
+print("The animal at 1 is: {}".format(animals[1]))
+print("The third animal is: {}".format(animals[2]))
+print("The first animal is: {}".format(animals[0]))
+print("The animal at 3 is: {}".format(animals[3]))
+print("The animal at 2 is: {}".format(animals[2]))
+print("The sixth animal is: {}".format(animals[5]))
+print("The animal at 4 is: {}".format(animals[4]))
